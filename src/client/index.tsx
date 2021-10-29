3a276341558f9732c07b197a334a46c4a5c9f3e7
Alexander Georgiev <aleksandar_georgiev@starsgroup.com>
2021-10-29 16:38:27 +0300

    Polish HMR

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -11,3 +11,10 @@ window.addEventListener("DOMContentLoaded", () => {
     document.getElementById("root")
   )
 })
+
+// TODO: Load app in docker
+// -- latest NODE LTS image with minimum dependencies
+// -- optimize docker build - do not run npm install if no new dependencies added
+// TODO: Setup local Jenkins with docker
+// TODO: react-router-config - https://subscription.packtpub.com/book/web_development/9781839215414/16/ch16lvl1sec91/server-side-rendering-with-data
+// TODO: Add Redux store
--- webpack.client.config.js
+++ webpack.client.config.js
@@ -18,6 +18,12 @@ const config = {
   devServer: {
     port: clientPort,
     liveReload: true,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers":
+        "X-Requested-With, content-type, Authorization",
+    },
   },
 }
 
